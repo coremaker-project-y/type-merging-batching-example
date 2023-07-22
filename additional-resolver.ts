cb38d425ca1dae67b491cc2c1ef7d8f0d0522aff
Cristian <ionutbriscaru@gmail>
2023-07-22 12:50:58 +0100

    fix bathcing ordering

---
+++ additional-resolver.ts
@@ -0,0 +1,38 @@
+import { print } from "graphql";
+import { Maybe, Resolvers } from './.mesh'
+
+const resolvers: Resolvers = {
+    AccessRightCompany: {
+        company: (root, _args, context, info) => {
+
+            const companiesIdsList:(Maybe<string> | undefined)[] = [];
+
+            return context.CompanyService.Query.listCompanies({
+                root,
+                context,
+                info,
+                key: root.id,
+                argsFromKeys: (companiesIds) => {
+                  
+                  if(companiesIds){
+                    companiesIdsList.push(...companiesIds)
+                  }
+                  
+                  return { input: { companiesIds } }
+                },
+                valuesFromResults: (data) => {
+                  
+                  if(!data || !data.companies) return [];
+
+                  return companiesIdsList.map( companyId => data.companies.find( company => company?.id === companyId))
+              
+                },
+                selectionSet: (companySelectionSet) => /* GraphQL */ `
+                    { companies ${print(companySelectionSet)} }
+                `,
+            });
+        },
+    },
+};
+
+export default resolvers
